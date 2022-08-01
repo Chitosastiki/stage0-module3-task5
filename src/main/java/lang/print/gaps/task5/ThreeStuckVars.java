17512bd6db9a3aba882985df346926a5446adcef
Chitosastiki <110228508+Chitosastiki@users.noreply.github.com>
2022-08-01 22:02:38 +0400

    Update ThreeStuckVars.java

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        
+        int first = 1;
+            int second = 10;
+            int third= 100;
+        for(int i; new int[]{first, second, third}){
+        System.out.println(i);
+        }
     }
 }
