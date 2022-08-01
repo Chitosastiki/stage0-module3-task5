5671938b1e52b9b01d2989e01748e49d99bb506c
Chitosastiki <110228508+Chitosastiki@users.noreply.github.com>
2022-08-01 22:18:40 +0400

    Update NumberDivider.java

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
         
-        float divided = toBeDivided / 5.0;
+        double divided = toBeDivided / 5.0;
         System.out.println(divided);
     }
 }
